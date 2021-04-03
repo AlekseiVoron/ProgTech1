a3a3bcca33fcef2cfc399f8f739ca22b6512674a
ThePeacemaker777 <IPIDlol777*>
2021-04-03 15:28:26 +0400

    Expand buffer to 100

--- src/ru/ssu/Constants.java
+++ src/ru/ssu/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String TEMP_DIR_NAME = "temp";
 
     public static final long RECOMMENDED_FREE_MEMORY = 16 * 1024 * 1024;   // 16 Mb
-    public static final int STR_FROM_ONE_FILE = 10;
+    public static final int STR_FROM_ONE_FILE = 100;
 
     public static final String AVAILABLE_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 }
